00d1ed770be099b01b053c92811e64c35498e934
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2021-12-22 04:10:47 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import { useEffect ,useState} from 'react';
+import {getTokenFromUrl} from "./spotify"
+import SpotifyWebApi from "spotify-web-api-js";
+
+import {useDataLayerValue} from "./DataLayer";
+
+import Player from './Player';
+
+const spotify=new SpotifyWebApi();
 
 function App() {
+  const [{user,token},dispatch]=useDataLayerValue();
+   
+  useEffect(()=>{
+    //set token
+    const hash=getTokenFromUrl()
+    window.location.hash="";
+    const _token=hash.access_token;
+
+    if(_token){
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user=>{
+        dispatch({
+          type:'SET_USER',
+          user:user,
+        })
+      });
+     
+
+    
+
+      spotify.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+      dispatch({
+        type: "SET_DISCOVER_WEEKLY",
+        discover_weekly: response,
+      })
+    );
+
+    
+    spotify.getMyTopArtists().then((response) =>
+    dispatch({
+      type: "SET_TOP_ARTISTS",
+      top_artists: response,
+    })
+  );
+
+  dispatch({
+    type: "SET_SPOTIFY",
+    spotify: spotify,
+  });
+
+  spotify.getMe().then((user) => {
+    dispatch({
+      type: "SET_USER",
+      user:user,
+    });
+  });
+
+  spotify.getUserPlaylists().then((playlists) => {
+    dispatch({
+      type: "SET_PLAYLISTS",
+      playlists,
+    });
+  });
+
+  
+    }
+  },[token,dispatch]);
+
+ console.log("user "+user)
+ console.log("token "+token)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token ? (
+          <Player spotify={spotify}/>
+        ):(
+          <Login />
+        )       
+       
+      }
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Login.css";
+import { accessUrl } from "./spotify";
+
+const Login = () => {
+    return (
+        <div className='login'>
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+             <a href={accessUrl}>LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
